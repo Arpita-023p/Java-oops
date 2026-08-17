b01b237f5bf6a2342cb650b05e1280efe3999af7
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-08-17 22:11:01 +0530

    calling a static method and pass primitive argument

---
+++ Staticnonprimtive.java
@@ -0,0 +1,8 @@
+public class Staticnonprimtive {
+    public static void m1(Staticnonprimtive s){
+        System.out.println("non primtive argument");
+    }
+    public static void main(String[] args) {
+        m1(new Staticnonprimtive());
+ }
+}
