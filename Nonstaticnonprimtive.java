7b294afb3fc9843b7d702f2df59d7c14a0dc8347
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-08-17 22:07:38 +0530

    calling a non static method as non primitive argument

---
+++ Nonstaticnonprimtive.java
@@ -0,0 +1,9 @@
+public class Nonstaticnonprimtive {
+    public void displaydetils(String name){
+        System.out.println("name is "+name);
+    }
+    public static void main(String[] args) {
+        Nonstaticnonprimtive obj=new Nonstaticnonprimtive();
+      obj.displaydetils("arpita");
+    }
+}
