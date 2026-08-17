e78ef25d8fed995e5a458dd5b4476cd7f8464396
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-08-17 22:09:18 +0530

    The primitive return type

---
+++ Primtivereturn.java
@@ -0,0 +1,9 @@
+public class Primtivereturn {
+    public static void main(String[] args) {
+        m1();
+    }
+    public static int m1(){
+        System.out.println("m1-method");
+        return 10;
+    }
+}
